e302e294a29d58ec538cfaf0c11431d20c379a52
MaxGolden <maxgold226@gmail.com>
2018-10-22 18:06:00 -0500

    delete

--- IonicApp1/src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG =  {
-  apiKey: "Your Api Key",
-  authDomain: "YourName.firebaseapp.com",
-  databaseURL: "https://asemax226.firebaseio.com",
-  projectId: "Your Project ID",
-  storageBucket: "YourName.appspot.com",
-  messagingSenderId: "Your ID"
-};
